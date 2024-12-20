2000128129ef6b8194f8d65ca0e6389e73e09d67
Ellis Francis <ellisfrancis@Elliss-MacBook-Pro.local>
2024-12-20 15:56:51 +0700

    last 自动发布

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/Zetra-Vue-WEB/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
